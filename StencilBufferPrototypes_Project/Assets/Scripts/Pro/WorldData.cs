5e66e8ec85b1afa0a903e04dab00a179c6bb0fcf
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2026-03-10 10:32:59 +0100

    World data organized in prefabs

--- StencilBufferPrototypes_Project/Assets/Scripts/Pro/WorldData.cs
+++ StencilBufferPrototypes_Project/Assets/Scripts/Pro/WorldData.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class WorldData : MonoBehaviour
 {
+    [Header("References")]
 
+    [SerializeField] private Camera cam;
+
+    [Tooltip("Referencia a la mascara cubo de la camara de este mundo")]
+    [SerializeField] private GameObject cubeMask;
+
+    [Tooltip("Controla como de cerca esta la mascara del cubo de la camara, tiene el pivote en la mascara del cubo")]
+    [SerializeField] private Transform cubeMaskZoom;
+
+    [Tooltip("Controla como de cerca esta la camara del mundo, manteniendo igual al cubo mascara, tiene el pivote en el target de la camara")]
+    [SerializeField] private Transform worldZoom;
 }
---
+++ StencilBufferPrototypes_Project/Assets/Scripts/Pro/WorldTransitionManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class WorldTransitionManager : MonoBehaviour
+{
+    [SerializeField] WorldData fromWorld;
+    [SerializeField] WorldData toWorld;
+
+
+}
